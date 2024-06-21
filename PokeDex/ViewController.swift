eaab0ea8f2cae0f93cd655662d02c03fb77a4795
_bo0od_ <bobson8894@gmail.com>
2024-06-21 22:01:21 +0800

    Initial Commit

---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Bo Chiu on 2024/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
