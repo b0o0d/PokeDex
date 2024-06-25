81020b6cbb7f76253a7fc07e7ff47e2750d3e28b
_bo0od_ <bobson8894@gmail.com>
2024-06-25 16:39:19 +0800

    add unit test for PokemonRepository

---
+++ PokeDexTests/PokemonRepositoryTests.swift
@@ -0,0 +1,37 @@
+//
+//  PokemonRepositoryTests.swift
+//  PokeDexTests
+//
+//  Created by Bo Chiu on 2024/6/24.
+//
+
+import XCTest
+@testable import PokeDex
+
+final class PokemonRepositoryTests: XCTestCase {
+    func testRepository() async throws {
+        let inMemory = CoreDataStack(inMemory: true)
+        let store = PokemonCoreDataStoreService(coreDataStack: inMemory)
+        let apiClient = PokemonAPIClient()
+        let sut = PokemonRepository(storeService: store, apiClient: apiClient)
+        
+        let expectation = XCTestExpectation(description: "Load Pokemon Display List")
+        
+        var times = 5
+        var count = sut.displayables.count
+        
+        let oriTimes = times
+        let oriCount = count
+        
+        while times > 0 {
+            try await sut.loadPokemonDisplayList()
+            times -= 1
+            XCTAssert(sut.displayables.count == count + sut.limit)
+            count = sut.displayables.count
+        }
+        XCTAssert(sut.displayables.count == oriCount + sut.limit * oriTimes)
+        expectation.fulfill()
+        
+        await fulfillment(of: [expectation], timeout: 10)
+    }
+}
