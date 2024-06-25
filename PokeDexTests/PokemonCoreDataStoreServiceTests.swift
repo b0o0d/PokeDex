f306c6df96094be9f462940582772e2f9c096251
_bo0od_ <bobson8894@gmail.com>
2024-06-25 13:54:56 +0800

    update test case of store service

--- PokeDexTests/PokemonCoreDataStoreServiceTests.swift
+++ PokeDexTests/PokemonCoreDataStoreServiceTests.swift
@@ -9,63 +9,43 @@ import XCTest
 @testable import PokeDex
 
 final class PokemonCoreDataStoreServiceTests: XCTestCase {
-    var sut: PokemonCoreDataStoreService?
-
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        let inMemory = CoreDataStack(inMemory: true)
-        sut = PokemonCoreDataStoreService(coreDataStack: inMemory)
-        try sut?.deleteAll()
-        sut?.saveContext()
-    }
-
-    override func tearDownWithError() throws {
-        try sut?.deleteAll()
-        sut?.saveContext()
-        sut = nil
-        print("tear down")
-        try super.tearDownWithError()
-    }
-
     func testCoreDataAddFetch() async throws {
-        let offset = 1024
-        let limit = 278
-        let response = await PokemonAPIClient.fetchRoughPokemons(offset: offset, limit: limit)
+        let apiClient = PokemonAPIClient()
+        let inMemory = CoreDataStack(inMemory: true)
+        let sut = PokemonCoreDataStoreService(coreDataStack: inMemory)
+        try sut.deleteAll()
+        
+        let offset = 0
+        let limit = 1302
+        let response = await apiClient.fetchRoughPokemons(offset: offset, limit: limit)
         XCTAssertNotNil(response)
         XCTAssertTrue(response!.results.count == limit)
         
+        var times = 0
         var addeds: [CoreDataPokemonDisplay] = []
         for result in response!.results {
             XCTAssertTrue(result.id != 0)
-            let detail = await PokemonAPIClient.fetchPokemonDetail(id: result.id)
+            let detail = await apiClient.fetchPokemonDetail(id: result.id)
             XCTAssertNotNil(detail)
-            let species = await PokemonAPIClient.fetchPokemonSpecies(url: detail!.species.url)
+            let species = await apiClient.fetchPokemonSpecies(url: detail!.species.url)
             XCTAssertNotNil(species)
             let enLastFlavorText = species?.flavorTextEntries.last { $0.language == "en" }
             XCTAssertNotNil(enLastFlavorText)
-            try? sut?.addPokemonFlavorText(enLastFlavorText!, speciesID: detail!.species.id)
-            let evolutionChain = await PokemonAPIClient.pokemonEvolutionChain(url: species!.evolutionChainURL)
+            try? sut.addPokemonFlavorText(enLastFlavorText!, speciesID: detail!.species.id)
+            let evolutionChain = await apiClient.pokemonEvolutionChain(url: species!.evolutionChainURL)
             XCTAssertNotNil(evolutionChain)
-            try? sut?.addPokemonEvolutionChain(evolutionChain!)
-            sut?.saveContext()
+            try? sut.addPokemonEvolutionChain(evolutionChain!)
             
-            let display: PokemonDisplay? = PokemonDisplayAdapter.adapt(pokemonDetail: detail!, pokemonEvolutionChain: evolutionChain!, pokemonSpecies: species!)
+            let display: PokemonDisplay? = PokemonDisplayAdapter.adapt(pokemonDetail: detail!, pokemonEvolutionChain: evolutionChain!, flavorText: enLastFlavorText!)
             XCTAssertNotNil(display)
-            let added = try? sut?.addPokemonDisplay(display!)
+            let added = try? sut.addPokemonDisplay(display!)
             if let added = added {
                 addeds.append(added)
             }
+            times += 1
+            print("\(times)/\(limit), \(addeds.count)")
         }
-        sut?.saveContext()
-        for added in addeds {
-            let display = added.toPokemonDisplay()
-            XCTAssertNotNil(display)
-            XCTAssertNotNil(display?.coreDataObjectID)
-        }
-        
-        let fetched = try sut?.fetchPokemonDisplayList(offset: 0, limit: 10000)
-        XCTAssertNotNil(fetched)
-        XCTAssertTrue(fetched!.count <= limit)
+        XCTAssert(addeds.count == 1025)
     }
 
 }
